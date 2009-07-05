2194f86d8c2245f12afaaeb9cf99540d6f007c5c
ray <ray@utopia.(none)>
2009-07-05 11:27:22 +0100

    added basic basic logging and implemented feed function exception handling into business logic

--- podBlaster.py
+++ podBlaster.py
@@ -1,8 +1,13 @@
 import feedFunctions,dbFunctions,os
+from datetime import datetime
 #make new objects
 db = dbFunctions.dbFunctions()
 pod = feedFunctions.feedFunctions()
-
+deviceID='00:1F:00:B6:21:C0'
+#start logging
+log=open('log.txt','a');
+t=datetime.now()
+log.write(str(t.day)+'/'+str(t.month)+'/'+str(t.year)+' '+str(t.hour)+':'+str(t.minute)+'\n')
 #first get feeds from db
 feedList=[]
 feeds=db.get('feed')
@@ -17,32 +22,40 @@ for feed in feedList:
 	#iterate through entries entering new episode in db if not already
 	for entry in entries:
 		enclosure=pod.getEnclosure(entry)
-		episode=pod.getEpisode(enclosure)
-		path=feedDirectory+episode
-		#Add new podcats into db with and set to download and transfer
-		print episode
-		if db.inDb(episode) == False:
-			db.addEpisode(feed[0],episode,enclosure)
+		if enclosure is not False:
+			episode=pod.getEpisode(enclosure)
+			path=feedDirectory+episode
+			#Add new podcats into db with and set to download and transfer
+			print episode
+			if db.inDb(episode) == False:
+				db.addEpisode(feed[0],episode,enclosure)
 			
 #download marked episodes
 downloads=db.getDownloadList()
 if downloads:
 	for download in downloads:
-		print download[1],download[2]
+		log.write(download[1]+download[2])
 		if pod.getDownload(download[1],download[2]):
 			db.setDownloaded(download[0])
-							
-
+			log.write(' downloaded \n')
+		else:
+			log.write(' download failed \n')					
+else:
+	log.write('No new downloads found \n')
 
 
 # get list of downloads to be transferred 
 downloads=db.getTransferList()
 	#if there are items in the queue and the bluetooth device is present attempt to transfer
 if downloads:
-	#get device details mac device id and download path
 	for download in downloads:
-		device=db.getDevice()
-		print 'obexftp -b '+device[2]+' -c '+device[3]+' -p '+download[1]
-		if os.system('obexftp -b '+device[2]+' -c '+device[3]+' -p '+download[1]) == 0:
+		print 'obexftp -b '+deviceID+' -c E:/Podcasts/ -p '+download[1]
+		log.write(download[1])
+		if os.system('obexftp -b '+deviceID+' -c E:/Podcasts/ -p '+download[1]) == 0:
 			db.setTransfered(download[0])
-
+			log.write(' transferred \n')
+		else:
+			log.write(' transfer failed \n')
+else:
+	log.write('No new downloads found \n')
+log.close()
